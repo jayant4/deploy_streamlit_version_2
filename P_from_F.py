cb1b4cf80a272705e5a24f1f8a6081d311315193
jayant4 <62827014+jayant4@users.noreply.github.com>
2020-08-22 09:37:05 +0530

    Update P_from_F.py

--- P_from_F.py
+++ P_from_F.py
@@ -1,13 +1,13 @@
-import scipy.stats as stats
-from rpy2 import robjects
+# import scipy.stats as stats
+# from rpy2 import robjects
 
 
 
 
-pf_val=robjects.r['pf']
-x=pf_val(1.74,2,2)
-print([1-y for y in x ])
-# working in R
-# In R, you can calculate p-value by:
-# 1-pf(Fvalue, df1=v1, df2=v2)
-# Hope this helps.
+# pf_val=robjects.r['pf']
+# x=pf_val(1.74,2,2)
+# print([1-y for y in x ])
+# # working in R
+# # In R, you can calculate p-value by:
+# # 1-pf(Fvalue, df1=v1, df2=v2)
+# # Hope this helps.
